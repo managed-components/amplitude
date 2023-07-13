af23be31f8b53741ae8650cf8c4e2f452148f501
Yo'av Moshe <yoav@cloudflare.com>
2023-07-13 17:34:43 +0200

    use const for some variables

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,8 @@ export default async function (manager: Manager, settings: ComponentSettings) {
   // maps ecommerce data: ampliteude handles only transaction data (order completed/Refunded), the rest of the events will be just added to the event_properties object like any other event, but without the need for triggers)
 
   const ecomDataMap = (event: MCEvent) => {
-    let { payload, type, name } = event
+    const { type, name } = event
+    let { payload } = event
     payload = { ...payload, ...payload.ecommerce }
     delete payload.ecommerce
     if (type === 'ecommerce') {
